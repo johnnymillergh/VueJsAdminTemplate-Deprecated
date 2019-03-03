d783c5271421d465f9d939ee90422dd46f9ad5a5
Johnny Miller <johnnysviva@outlook.com>
2019-03-03 22:35:56 +0800

    🆕 Add dependencies: axios, element-ui, js-cookie, vue, vue-router, vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Cookies from 'js-cookie'
+
+Vue.use(ElementUi, {
+  // set element-ui default size
+  size: Cookies.get('size') || 'mini'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
